fdb751ec9a5275ae8830b9c2b1bff97ad4766b7b
paulo-hortelan <paulo.ribeiro@aonet.com.br>
2024-10-24 17:03:24 -0300

    fix: naming

--- src/Facades/LaraZabbixFacade.php
+++ src/Facades/LaraZabbixFacade.php
@@ -3,11 +3,12 @@
 namespace PauloHortelan\LaraZabbix\Facades;
 
 use Illuminate\Support\Facades\Facade;
+use PauloHortelan\LaraZabbix\LaraZabbixClass;
 
 class LaraZabbixFacade extends Facade
 {
     protected static function getFacadeAccessor()
     {
-        return 'PauloHortelan\\LaraZabbix\\LaraZabbixClass';
+        return LaraZabbixClass::class;
     }
 }
